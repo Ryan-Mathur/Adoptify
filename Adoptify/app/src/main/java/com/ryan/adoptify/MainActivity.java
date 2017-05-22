83dfa4c5121834a5f86c86af53eb79bace821a5b
Ryan Mathur <ryanmathur7@gmail.com>
2017-05-22 15:30:42 -0400

    uploading to be current

--- Adoptify/app/src/main/java/com/ryan/adoptify/MainActivity.java
+++ Adoptify/app/src/main/java/com/ryan/adoptify/MainActivity.java
@@ -3,8 +3,8 @@ package com.ryan.adoptify;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -12,13 +12,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.ryan.adoptify.activity_test_sample.PetRecyclerAdapterTest;
 import com.ryan.adoptify.constants.PetFinderAPI;
 import com.ryan.adoptify.interfaces.PetFindInterface;
 import com.ryan.adoptify.objects.petfind.Breed;
@@ -31,8 +28,6 @@ import com.ryan.adoptify.recyclerview.PetRecyclerAdapter;
 
 import java.util.ArrayList;
 
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
